e42f5731153f3be77f46ca54db0694ce6571bced
Zhu Ziqi <kressety@163.com>
2025-05-05 17:05:04 +0800

    fix: update version number to 1.0.2 in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ftb-quest-localizer",
-    version="1.0.1",
+    version="1.0.2",
     description="FTB Quest Localization Helper",
     long_description=long_description,
     long_description_content_type="text/markdown",
